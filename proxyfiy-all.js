ed229c1b3b74d0ec75c93f521d7fec0856d1aea0
Frederik B <fbraun+gh@mozilla.com>
2014-07-09 12:57:17 +0200

    tiny proxy changes

--- proxyfiy-all.js
+++ proxyfiy-all.js
@@ -5,10 +5,12 @@ var proxyPrefs = Components.classes["@mozilla.org/preferences-service;1"]
          .getService(Components.interfaces.nsIPrefService)
          .getBranch("network.proxy.");
 
-var host='192.168.1.13'; //XXX change
+var host='127.0.0.1'; //XXX change
 var port=8080;
 
 proxyPrefs.setIntPref("type", 1);
 proxyPrefs.setCharPref("http", host)
 proxyPrefs.setIntPref("http_port", port);
 proxyPrefs.setBoolPref("share_proxy_settings", true);
+
+console.log("Please restart b2g (or reboot) for the proxy changes to take effect");
